622bfbe8576b39c6a8c1e98a8d881145df43fd62
jh1992jh <janihanninen@hotmail.com>
2018-08-10 02:37:49 +0300

    prepairing to deploy

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
+
 
 const app = express();
 
@@ -25,9 +27,17 @@ require('./config/passport')(passport);
 app.use('/api/users', users);
 app.use('/api/todos', todos);
 
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
+
 app.get('/', (req, res) => {
   res.json({ msg: 'The root route works' });
 });
 
-const PORT = 5000;
-app.listen(PORT, console.log(`The server is running on port ${PORT}`));
+const port = process.env.PORT || 5000;
+app.listen(port, console.log(`The server is running on port ${port}`));
